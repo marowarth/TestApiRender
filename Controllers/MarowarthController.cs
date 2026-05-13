0347dbdf3cbee6337d22d8444bdd6b0331f3cab3
francisco181288 <marowarth@gmail.com>
2026-05-13 12:59:51 +0200

    Cambios -habilitado swagger -añadido controlador

---
+++ Controllers/MarowarthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApiRender.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarowarthController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from MarowarthController!";
+        }
+    }
+}
